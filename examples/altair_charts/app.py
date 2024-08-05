7b4f1499636cf5ab6545f83b823af03672e11f68
Isaac Flath <Isaac.flath@gmail.com>
2024-08-05 12:59:16 -0400

    bug fix

--- examples/altair_charts/app.py
+++ examples/altair_charts/app.py
@@ -14,14 +14,17 @@ def generate_chart():
     chart = alt.Chart(pltr).mark_line().encode(x='x', y='y').properties(width=400, height=200)
     return altair2fasthml(chart)
 
-@app.get("/")
-def home():
+def homepage():
     return Title("Altair Demo"), Main(
         H1("Altair Demo"),
         Div(f"You have pressed the button {count} times.", id="chart"),
         Button("Increment", hx_get="/altair_charts/increment", hx_target="#chart", hx_swap="innerHTML"),
         style="margin: 20px"
     )
+
+@app.get("/")
+def home():
+    return homepage()
 
 
 @app.get("/increment")
@@ -33,5 +36,3 @@ def increment():
         generate_chart(),
         P(f"You have pressed the button {count} times."),
     )
-
-serve()
