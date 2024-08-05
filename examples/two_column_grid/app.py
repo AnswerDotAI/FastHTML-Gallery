550e2b21c0407071111bb56c0c736ca0e9e86df3
Isaac Flath <Isaac.flath@gmail.com>
2024-08-05 11:21:46 -0400

    Update app.py

--- examples/two_column_grid/app.py
+++ examples/two_column_grid/app.py
@@ -1,6 +1,9 @@
 from fasthtml.common import *
-app,rt = fast_app()
-
+links = [
+    Link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css", type="text/css"),
+    HighlightJS(langs=['python', 'javascript', 'html', 'css']),
+]
+app, rt = fast_app(hdrs=links)
 
 def homepage():
     return Titled('HTMX Form Demo', Grid(
