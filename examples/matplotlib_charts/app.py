b3f628fb10d48276dc641ffe2de461d498b7e2ab
Isaac Flath <Isaac.flath@gmail.com>
2024-08-05 10:24:08 -0400

    Update app.py

--- examples/matplotlib_charts/app.py
+++ examples/matplotlib_charts/app.py
@@ -20,8 +20,9 @@ def generate_chart():
 
 
 def homepage():
-    global count
+    global plotdata, count
     count = 0
+    plotdata = []
     return Div(Div(f"You have pressed the button {count} times.", id="chart"),
         Button("Increment", hx_get="/matplotlib_charts/increment", hx_target="#chart", hx_swap="innerHTML"))
 
