39903acf769a28ec58900ab705ec70020695753a
Isaac Flath <Isaac.flath@gmail.com>
2024-08-06 13:36:29 -0400

    New example

---
+++ examples/click_to_edit/app.py
@@ -0,0 +1,59 @@
+from fasthtml.common import *
+app, rt = fast_app()
+
+def contact_view(firstName: str, lastName: str, email: str, inputs=False):
+    if inputs:
+        vals = zip(['firstName', 'lastName', 'email'], [firstName, lastName, email])
+        firstName, lastName, email = [ Input(type='text', name=o, value=v) for o, v in vals ]
+    
+    return Div(
+        Div(Label(Strong('First Name: '), firstName)),
+        Div(Label(Strong('Last Name: '), lastName)),
+        Div(Label(Strong('Email: '), email)),
+        Button('Click To Edit', 
+               hx_get=f'/click_to_edit/form?firstName={firstName}&lastName={lastName}&email={email}', 
+               cls='btn primary'),
+        hx_target='this',
+        hx_swap='outerHTML'
+    )
+
+@app.get('/')
+def homepage():
+    c = {
+        'firstName': 'Joe',
+        'lastName': 'Blow',
+        'email': 'joe@blow.com'
+    }
+    return contact_view(**c)
+
+
+@app.get('/form')
+def edit_contact(firstName: str, lastName: str, email: str):
+    return Form(
+        Div(
+            Label('First Name'),
+            Input(type='text', name='firstName', value=firstName)
+        ),
+        Div(
+            Label('Last Name'),
+            Input(type='text', name='lastName', value=lastName),
+        ),
+        Div(
+            Label('Email Address'),
+            Input(type='email', name='email', value=email),
+        ),
+        Button('Submit', cls='btn'),
+        Button('Cancel', hx_get=f'/click_to_edit/contact?firstName={firstName}&lastName={lastName}&email={email}', cls='btn'),
+        hx_put='/click_to_edit/contact',
+        hx_target='this',
+        hx_swap='outerHTML'
+    )
+
+@app.put('/contact')
+def update_contact(firstName: str, lastName: str, email: str):
+    return contact_view(firstName, lastName, email)
+
+@app.get('/contact')
+def view_contact(firstName: str, lastName: str, email: str):
+    return contact_view(firstName, lastName, email)
+
---
+++ examples/click_to_edit/text.md
@@ -0,0 +1,3 @@
+# Click to Edit Button
+
+This is a simple example of a click to edit button based on the [HTMX Example](https://htmx.org/examples/click-to-edit/).
