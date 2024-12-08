6af233aecfd5ddff274a853b488d0d6345f02af6
Isaac Flath <Isaac.flath@gmail.com>
2024-12-08 07:44:11 -0500

    Minor tweaks

--- examples/dynamic_user_interface/custom_keybindings/app.py
+++ examples/dynamic_user_interface/custom_keybindings/app.py
@@ -1,21 +1,18 @@
-from fasthtml.common import fast_app, Div, Button
+from fasthtml.common import * 
 
 app, rt = fast_app()
 
+@rt
+def index():return Titled(
+    "Custom Keybindings with HTMX",
+    render_button("DO IT (Press `Shift + u`)"))
 
-@rt("/")
-def homepage():
-    return Div(render_button("DO IT (Shift + u)"))
-
-
-@rt("/doit")
-def doit():
-    return Div(render_button("😀 DID IT! "))
-
+@rt
+def doit(): return render_button("😀 DID IT! ")
 
 def render_button(text):
-    return Button(
-        text,
-        hx_trigger="click, keyup[key=='U'] from:body",
-        hx_post="/dynamic_user_interface/custom_keybindings/doit/",
-    )
+    return Button(text, 
+                  hx_trigger="click, keyup[key=='U'] from:body", 
+                  get=doit)
+
+serve()
