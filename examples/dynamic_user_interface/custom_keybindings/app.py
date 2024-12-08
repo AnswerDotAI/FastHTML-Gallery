ca1aa7ec7fa3dbb10a2325e082102a525bf3a6c8
Adnan <adnanmuhammedadnancp@gmail.com>
2024-12-08 09:34:49 +0530

    ref: simplify keybinding example

--- examples/dynamic_user_interface/custom_keybindings/app.py
+++ examples/dynamic_user_interface/custom_keybindings/app.py
@@ -1,36 +1,21 @@
-from fasthtml.common import fast_app, Div, Button, P
+from fasthtml.common import fast_app, Div, Button
 
 app, rt = fast_app()
-
-button_state = False
 
 
 @rt("/")
 def homepage():
-    return Div(render_button())
+    return Div(render_button("DO IT (Shift + u)"))
 
 
-@rt("/toggle")
-def toggle():
-    global button_state
-    button_state = not button_state
-    display_style = "block" if button_state else "none"
-    return Div(
-        render_button(),
-        Div(
-            P(
-                "FastHTML with HTMX lets you easily add custom keyboard shortcuts to your web app. Using the hx_trigger attribute, you can trigger actions like toggling content visibility with key combos, creating dynamic and responsive interfaces without page reloads."
-            ),
-            id="content",
-            style=f"display: {display_style}; text-align: center; margin-top: 10px;",
-        ),
-    )
+@rt("/doit")
+def doit():
+    return Div(render_button("😀 DID IT! "))
 
 
-def render_button():
+def render_button(text):
     return Button(
-        "Press Shift+u To Toggle!",
+        text,
         hx_trigger="click, keyup[key=='U'] from:body",
-        style="background-color: green; color: white; padding: 10px; margin: 10px; border: none;",
-        hx_post="/dynamic_user_interface/custom_keybindings/toggle/",
+        hx_post="/dynamic_user_interface/custom_keybindings/doit/",
     )
