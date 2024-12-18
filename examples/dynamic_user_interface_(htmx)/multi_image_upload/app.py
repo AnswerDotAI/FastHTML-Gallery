7ce0deb8b34117580390401de5abccffff1b5576
Isaac Flath <Isaac.flath@gmail.com>
2024-12-17 22:36:28 -0500

    Refactor

--- examples/dynamic_user_interface_(htmx)/multi_image_upload/app.py
+++ examples/dynamic_user_interface_(htmx)/multi_image_upload/app.py
@@ -6,57 +6,36 @@ app, rt = fast_app()
 @rt
 def index():
     inp = Card(
-        H3("Drag and drop images here"),
+        H3("Drag and drop images here", style="text-align: center;"),
         # HTMX for uploading multiple images
-        Input(
-        type="file",
-        name="images",
-        multiple=True,
-        required=True,
-        accept="image/*",
-        hx_post="/upload",
-        hx_target="#image-list",
-        hx_swap="afterbegin",
-        hx_trigger="change",
-        hx_encoding="multipart/form-data",
-    ), 
-    # Format upload box with white border so its clear where to drop the images
-    style="text-align: center; border: 2px solid #ccc; border-radius: 8px;",
-    )
-    # Custom CSS to put images in a responsive grid
-    image_list = Div(
-        id="image-list",
-        style="display: grid; grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); gap: 20px; padding: 20px;"
-    )
-    return Title("Multi Image Upload"), Div(
-        H1("Multi Image Upload"), 
+        Input(type="file",name="images", multiple=True, required=True, 
+              # Call the upload route on change
+              post=upload, hx_target="#image-list", hx_swap="afterbegin", hx_trigger="change",
+              # encoding for multipart
+              hx_encoding="multipart/form-data",accept="image/*"),
+        # Make a nice border to show the drop zone
+        style="border: 2px solid #ccc; border-radius: 8px;",)
+
+    return Titled("Multi Image Upload", 
         inp, 
         H3("👇 Uploaded images 👇", style="text-align: center;"),
-        image_list, 
-    )
+        Div(id="image-list"))
+
+
+async def ImageCard(image):
+    contents = await image.read()
+    # Create a base64 string
+    img_data = f"data:{image.content_type};base64,{b64encode(contents).decode()}"
+    # Create a card with the image
+    return Card(H4(image.filename), Img(src=img_data, alt=image.filename))
 
 @rt
 async def upload(request: Request):
+    # get all results from the request
     form = await request.form()
+    # Get the images in a list
     images = form.getlist("images")
-    image_elements = []
-    for image in images:
-        # Convert the image to base64 for image display
-        contents = await image.read()
-        img_data = f"data:{image.content_type};base64,{b64encode(contents).decode()}"
-        # Card styling for a single image
-        card = Div(
-            Card(
-                H4(image.filename),
-                Img(
-                    src=img_data,
-                    alt=image.filename,
-                    style="width: 100%; height: 200px; object-fit: cover; margin: 0;",
-                ),
-                style="height: 100%; text-align: center;",
-            )
-        )
-        image_elements.append(card)
-    return image_elements
+    # Create a grid filled with 1 image card per image
+    return Grid(*[await ImageCard(image) for image in images])
 
 serve()
