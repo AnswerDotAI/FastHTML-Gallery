46a6469c4f8c816c400a45645d386eb5433946ce
fflath <felix@fflath.email>
2024-12-29 07:49:50 -0500

    draggable reordering

--- examples/todo_series/advanced/app.py
+++ examples/todo_series/advanced/app.py
@@ -7,7 +7,7 @@ db = database('advanced_todo.db')
 todos,users = db.t.todos,db.t.users
 if todos not in db.t:
     users.create(dict(name=str), pk='name')
-    todos.create(id=int, title=str, status=str, due=date, name=str, pk='id')
+    todos.create(id=int, title=str, description=str, status=str, due=date, name=str, pk='id')
 Todo,User = todos.dataclass(),users.dataclass()
 login_redir = RedirectResponse('/login', status_code=303)
 
@@ -18,35 +18,36 @@ def before(req, sess):
 
 bware = Beforeware(before, skip=[r'/favicon\.ico', r'/static/.*', r'.*\.css', '/login'])
 
-app, rt = fast_app(hdrs=Theme.slate.headers(),before=bware)
+app, rt = fast_app(hdrs=(SortableJS('.sortable'),Theme.slate.headers()),before=bware)
 
 def tid(id): return f'todo-{id}'
 
-def mk_input(**kw):
-    return  Form(Grid(min_cols=3)(
-                Input(id='new-title',name='title',placeholder='New Todo',  **kw),
-                Input(id='new-status', name='status',value="Not Started", hidden=True, **kw),
-                Input(id='new-due',  name='due', value=date.today(),       **kw),
-                Button("Add",cls=ButtonT.primary, post=insert_todo,
-                       hx_target='#todo-list',hx_swap='innerHTML')), 
-                id='todo-input', cls='mb-6')
+def mk_todo_form(todo=Todo(title=None, status="Not Started", due=date.today(), description=None, id=None), btn_text="Add"):
+    """Create a form for todo creation/editing with optional pre-filled values"""
+    inputs = [Input(id='new-title', name='title',value=todo.title, placeholder='New Todo'),
+              Input(id='new-status',  name='status', value=todo.status,  hidden=True),
+              Input(id='new-due',   name='due',  value=todo.due),
+              Input(id='new-description',  name='description', placeholder="Task Description")]
 
-# def mk_todo_list(): return Grid(*todos(order_by='due'), cols=1)
+    # If there is an ID use it for editing existing row in db
+    if todo.id: inputs.append(Input(id='new-id', name='id', value=todo.id, hidden=True))
+        
+    return Form(DivLAligned(
+        *inputs,
+        Button(btn_text, cls=ButtonT.primary, post=upsert_todo,hx_target='#todo-list', hx_swap='innerHTML')),
+        id='todo-input', cls='mb-6')
+
+
+
+def mk_archive_list(): return Grid(*todos(order_by='due',where="status='Archive'"), cols=1)
 
 def mk_todo_list():
-    def is_old(due):
-        dd = datetime.strptime(due, '%Y-%m-%d').date()
-        return date.today() <= dd
-    date_cards = []
-    for td in todos(select="distinct due",order_by='due',where="status != 'Archive'"):
-        new_card = [Card(*todos(where=f"due='{td.due}' and status='Not Started'"),header="Not Started",body_cls='space-y-2',cls=CardT.default if is_old(td.due) else CardT.danger, id=f'new-{td.due}')]
-        inprogress_card = [Card(*todos(where=f"due='{td.due}' and status='In Progress'"),header="In Progress",body_cls='space-y-2',cls=CardT.default if is_old(td.due) else CardT.danger, id=f'inprogress-{td.due}')]
-        done_cards = [Card(*todos(where=f"due='{td.due}' and status='Done'"),header="Done",body_cls='space-y-2',cls=CardT.default if is_old(td.due) else CardT.danger, id=f'done-{td.due}')]
-    
-        date_cards.append(Card(Grid(min_cols=3)(*new_card,*inprogress_card,*done_cards),header=td.due))
+    new_cards = Card(*todos(where=f"status='Not Started'"),header="Not Started",body_cls='space-y-2 sortable', id=f'new')
+    inprogress_cards = Card(*todos(where=f"status='In Progress'"),header="In Progress",body_cls='space-y-2 sortable', id=f'inprogress')
+    done_cards = Card(*todos(where=f"status='Done'"),header="Done",body_cls='space-y-2 sortable', id=f'done')
 
-    # date_cards = [Card(*todos(where=f"due='{td.due}'"),header=td.due,body_cls='space-y-2',cls=CardT.default if is_old(td.due) else CardT.danger) for td in todos(select="distinct due",order_by='due')]
-    return Div(*date_cards,id='todo-dates')
+    return Div(Grid(min_cols=3)(new_cards,inprogress_cards,done_cards),id='todo-kanban')
+
 
 @app.delete("/delete_todo", name='delete_todo')
 async def delete_todo(id:int):
@@ -57,6 +58,9 @@ async def delete_todo(id:int):
 # this is used to customize the html representation of the Todo object
 @patch
 def __ft__(self:Todo):
+    def is_old(due):
+        dd = datetime.strptime(due, '%Y-%m-%d').date()
+        return date.today() <= dd
     
     _targets = {'hx_target':f'#{tid(self.id)}', 'hx_swap':'outerHTML'}
 
@@ -71,20 +75,29 @@ def __ft__(self:Todo):
     next_status = {"Not Started": "Start", "In Progress": "Complete", "Done": "Archive", "Archive": "Archive"}
     status = Button(next_status[self.status],
                     hx_post=update_status.to(id=self.id).lstrip('/'),hx_swap='delete',hx_target=f'#{tid(self.id)}')
+    due_date = Strong(self.due)
+
     
-    return Card(DivLAligned(Strong(self.title, target_id='current-todo'), 
-                            edit,delete,status),
-                id=tid(self.id))
+    return Card(DivLAligned(Strong(self.title, target_id='current-todo'),due_date),
+                id=tid(self.id),cls= CardT.default if is_old(self.due) else CardT.danger,footer=DivLAligned(edit,delete,status))
 
 @rt
 async def index():
     logout = Button("Logout",cls=ButtonT.primary, hx_get="/logout", hx_target='body') 
-    return Titled('Todo List',logout,mk_input(),Div(mk_todo_list(),id='todo-list'))
+    return Titled(
+        'Todo List',
+        logout,
+        mk_todo_form(),
+        Div(mk_todo_list(),id='todo-list'),
+        Details(Summary(H1("Archived Todos")),mk_archive_list()(id='archive-list')))
+
 
 @rt 
-async def insert_todo(todo:Todo):
-    todos.insert(todo,replace=True)
-    return mk_todo_list(),mk_input()(hx_swap_oob='true',hx_target='#todo-input',hx_swap='outerHTML')
+async def upsert_todo(todo:Todo):
+    # Create/update a todo if there is content
+    if todo.title.strip(): todos.insert(todo,replace=True)
+    # Reload main page with updated database content
+    return mk_todo_list(),mk_todo_form()(hx_swap_oob='true',hx_target='#todo-input',hx_swap='outerHTML')
 
 @rt 
 async def toggle_done(id:int):
@@ -94,21 +107,14 @@ async def toggle_done(id:int):
 async def update_status(id:int):
     todo = todos.get(id)
     next_status = {"Not Started": "In Progress", "In Progress": "Done", "Done": "Archive"}
-    next_target = {"Not Started": "inprogress", "In Progress": "done", "Done": "archive"}
-    print(f'beforeend:#{next_target[todo.status]}-{todo.due}')
-    return Div(todos.update(Todo(id=id, status=next_status[todo.status])))(hx_swap_oob=f'beforeend:#{next_target[todo.status]}-{todo.due}')
+    next_target = {"Not Started": "inprogress", "In Progress": "done", "Done": "archive-list"}
+    
+    # return Div(todos.update(Todo(id=id, status=next_status[todo.status])))(hx_swap_oob=f'beforeend:#{next_target[todo.status]}')
 
+    todos.update(Todo(id=id, status=next_status[todo.status]))
+    return Div(mk_todo_list())(hx_swap_oob=f'outerHTML:#todo-kanban')
 @rt 
-async def edit_todo(id:int):
-    todo = todos.get(id)
-    return  Card(Form(DivLAligned(
-                Input(id='new-title',name='title',value=todo.title,required=True),
-                Input(id='new-id',  name='id', value=todo.id,hidden=True),
-                Input(id='new-done', name='done',value=todo.done, hidden=True),
-                Input(id='new-due',  name='due', value=todo.due),
-                Button("Save",cls=ButtonT.primary, post=insert_todo,
-                       hx_target='#todo-list',hx_swap='innerHTML')), 
-                id='todo-input', cls='mb-6'))
+async def edit_todo(id:int): return Card(mk_todo_form(todos.get(id), btn_text="Save"))
 
 @rt("/login")
 def get():
